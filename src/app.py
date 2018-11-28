d26861643ed11d919e9d1e5479ac04c07397bafa
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2018-11-28 00:45:58 -0200

    Improving create playlist func

--- src/app.py
+++ src/app.py
@@ -1,29 +1,35 @@
 import random
+
 import click
 import spotipy
+import spotipy.util as util
 from spotipy.oauth2 import SpotifyClientCredentials
-
-# https://github.com/wilddima/vodopad/tree/master/vodopad
 
 
 class Auth:
-    def __init__(self, cli_id='', cli_secret=''):
+    def __init__(self, user_id=None, cli_id=None, cli_secret=None):
+        self.user_id = user_id
         self.cli_id = cli_id
         self.cli_secret = cli_secret
 
     def authenticate(self):
-        client_credentials_manager = SpotifyClientCredentials(
-            client_id=self.cli_id, client_secret=self.cli_secret)
-        sp = spotipy.Spotify(
-            client_credentials_manager=client_credentials_manager)
-        return sp
+        scopes = 'playlist-modify-public playlist-read-collaborative'
+        token = util.prompt_for_user_token(
+            self.user_id, scope=scopes, client_id=self.cli_id, client_secret=self.cli_secret, redirect_uri='http://localhost:8888/callback/')
+
+        if token:
+            sp = spotipy.Spotify(auth=token)
+            sp.trace = False
+            return sp
 
 
 class Analyze:
-    def __init__(self, authenticator, user_id='', playlist_id='', score=0):
+    def __init__(self, authenticator, user_id='', playlist_user_id='', playlist_id='',  name='', score=0):
         self.authenticator = authenticator
         self.user_id = user_id
+        self.playlist_user_id = playlist_user_id
         self.playlist_id = playlist_id
+        self.name = name
         self.score = score
 
     def get_artist(self, list=[]):
@@ -58,12 +64,14 @@ class Analyze:
             list_tracks_id, len(list_tracks_id))
         return list_tracks_id_shuffle
 
-    def create_playlist():
-        # criar playlist e add na playlist (user_playlist_add_tracks).
+    def create_playlist(self):
+        playlist = self.authenticator.user_playlist_create(
+            self.user_id, self.name)
+        return playlist['id']
 
     def analyze(self):
         res = self.authenticator.user_playlist_tracks(
-            self.user_id, self.playlist_id, fields='next, items(track(popularity, artists(id)))')
+            self.playlist_user_id, self.playlist_id, fields='next, items(track(popularity, artists(id)))')
         tracks = res['items']
         while res['next']:
             res = self.authenticator.next(res)
@@ -73,12 +81,17 @@ class Analyze:
 
 
 @click.command()
-@click.option('--user', '-u', help='Insert a user id', default='12141429536', required=True)
-@click.option('--playlist', '-p', help='Insert a playlist id', default='6C9TO1dfZZQTHedI8Qv18p', required=True)
+@click.option('--user', '-u', help='Insert your user id', required=True)
+@click.option('--user-playlist-id', '-upi', help='Insert a user id of playlist owner', required=True)
+@click.option('--playlist', '-p', help='Insert a playlist id', required=True)
+@click.option('--name', '-n', help='Insert a playlist name', required=True)
 @click.option('--score', '-s', help='Insert a score 0/100 to get assorted artists in playlist', default=100, required=False)
-def main(user, playlist, score):
+def main(user, user_playlist_id, playlist, name, score):
     cli_id = '30046b20b1d443cf9a9b9175e82b0970'
     cli_sec = '02bdac6c364b4b7091cbd58248473738'
 
-    authenticate = Auth(cli_id, cli_sec).authenticate()
-    analyze = Analyze(authenticate, user, playlist, score).analyze()
+    authenticate = Auth(user, cli_id, cli_sec).authenticate()
+    analyze = Analyze(authenticate, user, user_playlist_id,
+                      playlist, name, score).analyze()
+
+# analyze -u tr6amda6xwmpllo403xl9lf9c -upi 12141429536 -p 6C9TO1dfZZQTHedI8Qv18p -n testando
